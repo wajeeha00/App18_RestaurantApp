774a5a5972d1c1d792ea2c4cf0618989aaa1710d
wajeeha00 <wajeehaaftab7890@gmail.com>
2024-08-08 12:45:08 +0500

    yay

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('meal',  'status')
+    list_filter = ( 'status',)
+    search_fields = ('meal', 'description')
+
+admin.site.register(Item, MenuItemAdmin)
--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -16,7 +16,12 @@ STATUS = (
 class Item(models.Model):
     meal = models.CharField(max_length=1000, unique = True)
     description = models.CharField(max_length=2000)
-    price = models.DecimalField(decimal_places=2)
-    meal_type= models.CharField(choice=MEAL_TYPE, max_length=1000)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    meal_type= models.CharField(choices=MEAL_TYPE, max_length=1000)
     author = models.ForeignKey(User,on_delete=models.PROTECT)
-    status = models.IntegerField(choices==STATUS, default=0)
+    status = models.IntegerField(choices=STATUS, default=1)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.meal
